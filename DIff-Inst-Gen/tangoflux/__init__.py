337b778ba3d29e224b947a23628dddf7c09691a7
James Brynes <deltastudios0@icloud.com>
2026-01-11 16:40:54 +0800

    Add files via upload

---
+++ DIff-Inst-Gen/tangoflux/__init__.py
@@ -0,0 +1,78 @@
+from diffusers import AutoencoderOobleck
+import torch
+from transformers import T5EncoderModel, T5TokenizerFast
+from diffusers import FluxTransformer2DModel
+from torch import nn
+from typing import List
+from diffusers import FlowMatchEulerDiscreteScheduler
+from diffusers.training_utils import compute_density_for_timestep_sampling
+import copy
+import torch.nn.functional as F
+import numpy as np
+from tangoflux.model_pitch_velocity import TangoFlux
+from huggingface_hub import snapshot_download
+from tqdm import tqdm
+from typing import Optional, Union, List
+from datasets import load_dataset, Audio
+from math import pi
+import json
+import inspect
+import yaml
+from safetensors.torch import load_file
+import random
+
+
+class TangoFluxInference:
+
+    def __init__(
+        self,
+        name="declare-lab/TangoFlux",
+        device="cuda" if torch.cuda.is_available() else "cpu",
+    ):
+
+        self.vae = AutoencoderOobleck()
+
+        paths = name
+        vae_weights = load_file("{}/vae.safetensors".format(paths))
+        self.vae.load_state_dict(vae_weights)
+        weights = load_file("{}/tangoflux.safetensors".format(paths))
+
+        with open("{}/config.json".format(paths), "r") as f:
+            config = json.load(f)
+        self.model = TangoFlux(config)
+        self.model.load_state_dict(weights, strict=False)
+        # _IncompatibleKeys(missing_keys=['text_encoder.encoder.embed_tokens.weight'], unexpected_keys=[]) this behaviour is expected
+        self.vae.to(device)
+        self.model.to(device)
+
+    def generate(
+        self,
+        prompt,
+        mulan,
+        steps=25,
+        duration=4,
+        pitch=60,
+        velocity=25,
+        guidance_scale=4.5,
+        seed=None,
+    ):
+
+        # 修改为
+        velocity = max(int(int(velocity/25)-1), 0)
+        velocity = min(velocity, 4)  # 使用已经计算的值
+        with torch.no_grad():
+            latents = self.model.inference_flow(
+                prompt,
+                pitch=[pitch],
+                velocity=[velocity],
+                mulan = mulan,
+                duration=duration,
+                num_inference_steps=steps,
+                guidance_scale=guidance_scale,
+                seed=seed if seed is not None else random.randint(1000, 1000000),
+            )
+
+            wave = self.vae.decode(latents.transpose(2, 1)).sample.cpu()[0]
+        waveform_end = int(duration * self.vae.config.sampling_rate)
+        wave = wave[:, :waveform_end]
+        return wave
